10993ac73cea7e22ea81da62f18acfad785aca85
Proteen Das <proteen.das@olamnet.com>
2024-03-12 13:18:29 +0530

    Title Change

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export const useTitle = (title) => {
 
     useEffect(() => {
-        document.title = `${title} | Tabzon`
+        document.title = `${title} | FakeKart`
     }, [title]);
 
     return null;
